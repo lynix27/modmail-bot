bb041bd66f5afb19dfb742911448d0cb70c4d99c
Lynix <102491441+lynix27@users.noreply.github.com>
2023-09-07 22:25:38 +0200

    added check before deletion if channel is a ticket channel

--- views/channelclosureconfirmation.py
+++ views/channelclosureconfirmation.py
@@ -18,14 +18,36 @@ class ChannelClosureConfirmation(discord.ui.View):
             autocommit=True
         )
         cursor = await conn.cursor()
+        await cursor.execute("SELECT CATEGORYID FROM ticket_categories WHERE SERVERID = %s", (interaction.guild.id,))
+        result1 = await cursor.fetchone()
+        if result1 is None:
+            await cursor.close()
+            conn.close()
+            await interaction.followup.send("❗ I am not set up yet. Please use `/setup` to do that.", ephemeral=True)
+            return
+        else:
+            try:
+                category = await interaction.guild.fetch_channel(result1[0])
+            except:
+                await cursor.close()
+                conn.close()
+                await interaction.followup.send("❗ The saved ticket channel category doesn't exist anymore! Please define a new one by using `/ticket-category`!")
+                return
+            if interaction.channel in category.channels:
+                pass
+            else:
+                await cursor.close()
+                conn.close()
+                await interaction.followup.send("❗ The channel you are in is not a ticket channel!")
+                return
         await cursor.execute("SELECT CHANNELID from log_channels WHERE SERVERID = %s", (interaction.guild.id,))
-        result = await cursor.fetchone()
-        if result is None:
+        result2 = await cursor.fetchone()
+        if result2 is None:
             await cursor.close()
             conn.close()
             pass
         else:
-            log_channel = interaction.guild.get_channel(int(result[0]))
+            log_channel = interaction.guild.get_channel(int(result2[0]))
             embed = discord.Embed(title="🗑️ Ticket has been deleted", description=f"The ticket channel for the user **{interaction.channel.name}** (**{interaction.channel.id}**) has been deleted by {interaction.user.mention} (**{interaction.user.id}**).")
             await cursor.close()
             conn.close()
